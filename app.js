f73c1c91605ccd55caff8f3817bc104cd5b7511a
Juliana Bustos <julibustosar@gmail.com>
2025-01-19 18:28:44 -0500

    Realicé la función para verificar nombres y que estos aparezcan en la lista HTML.

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-/* -Agregar nombres
-- Validar que se introduzca texto.
-- Visualizar la lista.
+/* - Visualizar la lista.
 - Sorteo de los valores ingresados. */
 
 let nombres = [];
 
+//Agregar nombres y validar que se introduzca texto.
 function introduccionDeNombres() {
 
     let nombresDeAmigos = document.getElementById('amigo').value;
 
-    if(nombresDeAmigos == '') {
+    if(nombresDeAmigos === '') {
         alert('Por favor inserte un nombre válido.');
     } else {
-        nombres.push(nombresDeAmigos);
-        limpiarCaja();
+         nombres.push(nombresDeAmigos);
+         limpiarCaja();
+        verificarNombres();
     }
+
     
     console.log(nombresDeAmigos);
 }
 
 function limpiarCaja() {
     document.getElementById('amigo').value = '';
+
+}
+
+//Visualizar la lista.
+
+function verificarNombres() {
+
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < nombres.length; i++) {
+        let nuevoNombre = document.createElement('li');
+        nuevoNombre.textContent = nombres[i];
+        lista.appendChild(nuevoNombre);
+    }
+
+
 
 }
